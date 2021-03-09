ee1aeb790ad7bceaae7499b30ad98a0879d51b50
Roma Labunskiy <romalabunskiy13@gmail.com>
2021-03-09 18:55:25 +0200

    конец туториала, приложение работает, готово к проверке

--- polls/models.py
+++ polls/models.py
@@ -8,10 +8,14 @@ class Question(models.Model):
     pub_date = models.DateTimeField('date published')
 
     def __str__(self):
-        return str(self.question_text)
+        return self.question_text
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)    
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 
 class Choice(models.Model):
@@ -21,3 +25,6 @@ class Choice(models.Model):
 
     def __str__(self):
         return str(self.choice_text)
+
+
+
